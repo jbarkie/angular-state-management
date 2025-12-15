f03eea733e6caf28d0f94226aca591e023bf9f88
Joe Barkie <Joseph_Barkie@progressive.com>
2025-12-15 16:37:56 -0400

    Lift state from component to payStore

--- src/frontend/projects/apps/class/domains/component-state/pages/signals/computed.ts
+++ src/frontend/projects/apps/class/domains/component-state/pages/signals/computed.ts
@@ -1,10 +1,5 @@
-import { Component, ChangeDetectionStrategy, signal, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { payStore } from './stores/pay';
-
-type PayInfo = {
-  hourlyRate: number;
-  hoursWorked: number;
-};
 
 @Component({
   selector: 'app-computed-signals',
@@ -14,14 +9,11 @@ type PayInfo = {
   viewProviders: [],
   template: `
     <p>Computed Signals Component</p>
-    <p>Your name is {{ myName() }}</p>
-    <button (click)="embiggenation()" class="btn btn-primary">Click me!</button>
-
     <div>
-      <p>Hourly rate: {{ info().hourlyRate }}</p>
-      <p>Number of hours: {{ info().hoursWorked }}</p>
-      <button (click)="add(1)" class="btn btn-secondary">Add 1 hour</button>
-      <p>Predicted paycheck: {{ totalPay() }}</p>
+      <p>Hourly rate: {{ store.hourlyRate() }}</p>
+      <p>Number of hours: {{ store.hoursWorked() }}</p>
+      <button (click)="store.add(1)" class="btn btn-secondary">Add 1 hour</button>
+      <p>Predicted paycheck: {{ store.totalPay() }}</p>
     </div>
   `,
   styles: ``,
@@ -32,32 +24,21 @@ export class ComputedSignals {
   // don't do this:
   // myName = "Joe"
 
-  myName = signal('Joe');
-
-  embiggenation() {
-    this.myName.update((old) => old.toUpperCase());
-  }
-
-  add(hours: number) {
-    this.info.update((payInfo) => ({
-      ...payInfo,
-      hoursWorked: payInfo.hoursWorked + hours,
-    }));
-  }
-
-  info = signal<PayInfo>({
-    hourlyRate: 10.5,
-    hoursWorked: 40,
-  });
+  // add(hours: number) {
+  //   this.info.update((payInfo) => ({
+  //     ...payInfo,
+  //     hoursWorked: payInfo.hoursWorked + hours,
+  //   }));
+  // }
 
   protected store = inject(payStore);
 
   // "don't type public, always refactor to it"
-  totalPay = computed(() => {
-    // create variables for all the signals you are going to use here first
-    // not required but a bit "preemptive"
-    const pay = this.info().hourlyRate;
-    const hours = this.info().hoursWorked;
-    return pay * hours;
-  });
+  // totalPay = computed(() => {
+  //   // create variables for all the signals you are going to use here first
+  //   // not required but a bit "preemptive"
+  //   const pay = this.info().hourlyRate;
+  //   const hours = this.info().hoursWorked;
+  //   return pay * hours;
+  // });
 }
