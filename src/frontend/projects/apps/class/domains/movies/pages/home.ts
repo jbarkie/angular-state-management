210c6d9a3d1857d54209b2173d47fa221cb5532f
Joe Barkie <Joseph_Barkie@progressive.com>
2025-12-16 13:35:38 -0400

    Fetch movies from API instead

--- src/frontend/projects/apps/class/domains/movies/pages/home.ts
+++ src/frontend/projects/apps/class/domains/movies/pages/home.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FeaturePage } from '@app-shell/features/feature-page';
 import { List } from './lists/list';
 import { ApiMovie } from './lists/types';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-movies-pages-home',
@@ -10,57 +11,18 @@ import { ApiMovie } from './lists/types';
   imports: [FeaturePage, DatePipe, List],
   template: `
     <ui-feature-page pageName="The Movies">
-      <app-movie-list [movies]="fakeMovies()"></app-movie-list>
+      @if (movies.hasValue()) {
+        <app-movie-list [movies]="movies.value()"></app-movie-list>
+      } @else {
+        @if (movies.isLoading()) {
+          <p>Loading movies...</p>
+        }
+      }
     </ui-feature-page>
   `,
   styles: ``,
 })
 export class HomePage {
-  fakeMovies = signal<ApiMovie[]>([
-    {
-      id: '1',
-      title: 'Inception',
-      releaseDate: '2010-07-16',
-      genre: 'sci-fi',
-      rating: 2,
-      director: 'Christopher Nolan',
-      cast: [
-        { role: 'Cobb', actor: 'Leonardo DiCaprio' },
-        { role: 'Arthur', actor: 'Joseph Gordon-Levitt' },
-      ],
-      duration: 148,
-      version: 1,
-    },
-    {
-      id: '2',
-      title: 'Star Wars: Episode IV - A New Hope',
-      releaseDate: '1977-05-25',
-      genre: 'sci-fi',
-      rating: 5,
-      director: 'George Lucas',
-      cast: [
-        { role: 'Luke Skywalker', actor: 'Mark Hamill' },
-        { role: 'Darth Vader', actor: 'David Prowse' },
-        { role: 'Princess Leia', actor: 'Carrie Fisher' },
-        { role: 'Han Solo', actor: 'Harrison Ford' },
-      ],
-      duration: 152,
-      version: 1,
-    },
-    {
-      id: '3',
-      title: 'Twin Peaks: Fire Walk with Me',
-      releaseDate: '1992-05-23',
-      genre: 'drama',
-      rating: 5,
-      director: 'David Lynch',
-      cast: [
-        { role: 'Dale Cooper', actor: 'Kyle MacLachlan' },
-        { role: 'Laura Palmer', actor: 'Sheryl Lee' },
-        { role: 'Bob Billings', actor: 'Michael J. Anderson' },
-      ],
-      duration: 90,
-      version: 1,
-    },
-  ]);
+  // type safety - late bound call (no build error, maybe runtime error)
+  movies = httpResource<ApiMovie[]>(() => '/api/movies');
 }
