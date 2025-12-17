d60ed10c242e8e241d2398dc8d78cd78b0c6e8d6
Joe Barkie <Joseph_Barkie@progressive.com>
2025-12-17 13:44:11 -0400

    Remove handlers

--- src/frontend/projects/apps/class/src/mocks/handlers.ts
+++ src/frontend/projects/apps/class/src/mocks/handlers.ts
@@ -1,4 +1,2 @@
 import { HttpHandler } from 'msw';
-import { bffHandlers } from './bff/handlers';
-import { movieHandlers } from './demos/moviesHandler';
-export const handlers: HttpHandler[] = [...bffHandlers, ...movieHandlers];
+export const handlers: HttpHandler[] = [];
